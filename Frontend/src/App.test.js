ab563ec121a6cd0fc0c0a74ad2ee73e4d864509e
dev-awaism <awaismanzoor825@gmail.com>
2024-10-04 10:00:00 -0500

    feat: add tests

--- Frontend/src/App.test.js
+++ Frontend/src/App.test.js
@@ -6,3 +6,4 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+// Updated on Sun Jul  6 12:57:28 CDT 2025
